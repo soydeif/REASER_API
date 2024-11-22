722be98aa2af39242ee404cabccc4dd411e8519c
David Diaz Nava <81191528+soydeif@users.noreply.github.com>
2024-11-22 13:29:36 +0100

    feat-updateAllFeeds

--- deleteAllFeeds.js
+++ deleteAllFeeds.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 async function getAllFeeds() {
   try {
-    const response = await axios.get("http://localhost:23628/api/myfeeds");
+    const response = await axios.get("https://reaserapi-production.up.railway.app/api/myfeeds");
     return response.data; 
   } catch (error) {
     console.error("Failed to fetch feeds:", error.response ? error.response.data : error.message);
@@ -12,7 +12,7 @@ async function getAllFeeds() {
 
 async function deleteFeed(id) {
   try {
-    await axios.delete(`http://localhost:23628/api/deletefeed/${id}`);
+    await axios.delete(`https://reaserapi-production.up.railway.app/api/deletefeed/${id}`);
     console.log(`Deleted feed with ID: ${id}`);
   } catch (error) {
     console.error(`Failed to delete feed with ID: ${id}`, error.response ? error.response.data : error.message);
